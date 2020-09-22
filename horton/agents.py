1a1747437702d2687d9173ef3ee6675398304c94
Alexander Lavrov <alexander.lavrov@elastoo.com>
2020-09-23 01:06:42 +0300

    add value_type

--- horton/agents.py
+++ horton/agents.py
@@ -12,8 +12,10 @@ from horton.records import CollectSecurityOverview
 
 app = get_app()
 
-collect_securities_topic = app.topic("collect_securities", internal=True)
-collect_security_overview_topic = app.topic("collect_security_overview", internal=True)
+collect_securities_topic = app.topic("collect_securities", internal=True, partitions=2)
+collect_security_overview_topic = app.topic(
+    "collect_security_overview", internal=True, value_type=CollectSecurityOverview
+)
 
 
 @app.agent(collect_security_overview_topic)
@@ -22,7 +24,9 @@ async def collect_security_overview(
 ) -> AsyncIterable[bool]:
     async with aiohttp.ClientSession() as session:
         async for event in stream:
-            logger.info("Start collect security [{symbol}] overview", symbol=event.symbol)
+            logger.info(
+                "Start collect security [{symbol}] overview", symbol=event.symbol
+            )
 
             client = AlphaVantageClient(session, API_KEY)
 
@@ -45,20 +49,20 @@ async def collect_securities(stream: StreamT[None]) -> AsyncIterable[bool]:
 
             for security in securities:
                 await SecurityCRUD.update_one(
-                    {"symbol": security["symbol"], "exchange": security["exchange"]}, security, upsert=True
+                    {"symbol": security["symbol"], "exchange": security["exchange"]},
+                    security,
+                    upsert=True,
                 )
 
-                await collect_security_overview.cast(
+                """await collect_security_overview.cast(
                     CollectSecurityOverview(symbol=security["symbol"], exchange=security["exchange"])
-                )
+                )"""
 
             yield True
 
 
 @app.command()
 async def start_collect_securities():
-    """
-    Collect securities and overview
-    """
+    """Collect securities and overview."""
 
     await collect_securities.cast()
